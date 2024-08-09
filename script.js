699bf1f7331f08cdbb2817fc613b097278cf2948
gelaco1989 <gelaco1989@gmail.com>
2024-08-08 23:05:13 -0300

    implementação da soma de parede, botão zap, link whatsapp

--- script.js
+++ script.js
@@ -46,24 +46,47 @@ function f530(value) {
 }
 
 function parafusoGn25(value) {
-    var guia = 1.5
+    var parafusoGn = 15
     var result = eval(document.getElementById('display1').value);
-    document.getElementById('display2').value = result / guia ;
+    document.getElementById('display2').value = result * parafusoGn ;
 }
 
 function ParafusoChap(value) {
-    var guia = 1.5
+    var parafusoFlang = 15
     var result = eval(document.getElementById('display1').value);
-    document.getElementById('display2').value = result / guia ;
+    document.getElementById('display2').value = result * parafusoFlang ;
 }
 
 function regulador(value) {
     var guia = 1.5
     var result = eval(document.getElementById('display1').value);
-    document.getElementById('display2').value = result / guia ;
+    document.getElementById('display2').value = result * guia ;
 }
 
-function showList() {
+function showListTeto() {
+    
+    var placaStG =2
+    var cantoneira = 2.5
+    var f530 = 1.8
+    var tiranteERegulador = 1.3  
+    var parafusoGn = 5 
+    var parafusoFlang=0.15
+    var massa = 0.5 
+    var fita = 2
+    var result = eval(document.getElementById('display1').value);
+    
+    document.getElementById('BudgetTeto').style.display = 'block';
+    document.getElementById('displayPlaca').value = result / placaStG;
+    document.getElementById('displayCantoneira').value = result / cantoneira;
+    document.getElementById('displayF530').value = result / f530;
+    document.getElementById('displayTranteERegulor').value = result * tiranteERegulador;
+    document.getElementById('displayParafusoGn').value = result / parafusoGn +" Ct";
+    document.getElementById('displayParafusoFlang').value = result * parafusoFlang +" Ct";
+    document.getElementById('displayMassa').value = result * massa+" Kg";
+    document.getElementById('displayFita').value = result * fita + " Mt";
+}
+
+function showListParede() {
     
     var placaStG =2
     var guia = 0.75
@@ -75,7 +98,7 @@ function showList() {
     var fita = 2
     var result = eval(document.getElementById('display1').value);
     
-    document.getElementById('Budget').style.display = 'block';
+    document.getElementById('BudgetParede').style.display = 'block';
     document.getElementById('displayPlaca').value = result / placaStG;
     document.getElementById('displayGuia').value = result * guia;
     document.getElementById('displayMontante').value = result * montante;
@@ -84,20 +107,31 @@ function showList() {
     document.getElementById('displayParafusoFlang').value = result * parafusoFlang;
     document.getElementById('displayMassa').value = result * massa;
     document.getElementById('displayFita').value = result * fita;
-    
-    
-    
 }
 
-function closeList() {
+function closeListTeto() {
 
-    document.getElementById('Budget').style.display = 'none';
+    document.getElementById('BudgetTeto').style.display = 'none';
+    console.log('aqui')
+}
+
+function closeListParede() {
+
+    document.getElementById('BudgetParede').style.display = 'none';
     console.log('aqui')
 }
 
 function calculate() {
     var result = eval(document.getElementById('display1').value);
     document.getElementById('display1').value = result;
+}
+
+function print() {
+
+    document.getElementById("contact").addEventListener("click", function() {
+        window.print();
+    });
+
 }
 
 
